2110b2b05bc3dfb241cfeaaaf0a15a31f239c16e
xChocoCRISpis <crisneeda@gmail.com>
2024-12-03 21:10:41 -0600

    productsfuncionando

--- Common/VO/Models/Products.cs
+++ Common/VO/Models/Products.cs
@@ -12,24 +12,44 @@ namespace VO
     {
         [DataMember(EmitDefaultValue = false)]
         public int Id { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public string Name { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public string Description { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public double Price { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public int Current_Stock { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public int Max_Stock { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public int Min_Stock { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public eStockStatus Stock_Status_Id { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public string Image_Path { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public DateTime Created_At { get; set; }
+
+
         [DataMember(EmitDefaultValue = false)]
         public DateTime Updated_At { get; set; }
     }
