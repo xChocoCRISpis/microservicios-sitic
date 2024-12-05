66b538b82c850285008e0649ba32a02af889a7d7
xChocoCRISpis <crisneeda@gmail.com>
2024-12-05 17:23:14 -0600

    modificaciones a los pipes

--- sitic-commerce/src/app/shared/pipes/choice-class/choice-class.pipe.ts
+++ sitic-commerce/src/app/shared/pipes/choice-class/choice-class.pipe.ts
@@ -8,9 +8,9 @@ export class ChoiceClassPipe implements PipeTransform {
 
   transform(type:eChoiceType):string  {
     const typesScreen = new Map<eChoiceType, string> ([
-      [eChoiceType.None, 'border-gray-300 bg-gray-50 text-gray-700'],
-      [eChoiceType.Accept, 'border-blue-500 bg-blue-50 text-blue-700'],
-      [eChoiceType.Caution, 'border-red-500 bg-red-50 text-red-700']
+      [eChoiceType.None, 'border-gray-300 text-gray-700'],
+      [eChoiceType.Accept, 'border-blue-500 text-blue-700'],
+      [eChoiceType.Caution, 'border-red-500 text-red-700']
     ]);
 
     return typesScreen.has(type) ? typesScreen.get(type) : typesScreen.get(eChoiceType.None);
