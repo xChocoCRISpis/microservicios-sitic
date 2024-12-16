41371dd0bb4a05ac93dbce6779623471f6d345d2
xChocoCRISpis <crisneeda@gmail.com>
2024-12-16 13:46:28 -0600

    cart ya carga los items con servicions

--- sitic-commerce/src/app/modules/store/side-bar-cart/side-bar-cart.component.ts
+++ sitic-commerce/src/app/modules/store/side-bar-cart/side-bar-cart.component.ts
@@ -1,28 +1,71 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import { CartItem } from 'src/app/shared/interfaces/carts/cart-item/cart-item.interface';
-import { Product } from 'src/app/shared/interfaces/products/product.interface';
-import { cartItems, products } from '../side-bar-item/data-dummie';
-import { OutgoingMessage } from 'http';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { CartItem } from "src/app/shared/interfaces/carts/cart-item/cart-item.interface";
+import { Product } from "src/app/shared/interfaces/products/product.interface";
+/* import { cartItems, products } from "../side-bar-item/data-dummie"; */
+import { CartWithItems } from "src/app/shared/interfaces/carts/cart/cart-with-items.interface";
+import { ProductsService } from "src/app/shared/services/products.service";
+import { ProductsResponse } from "src/app/shared/interfaces/products/products-response.interface";
+import { eErrorType } from "src/app/shared/interfaces/comun/enums.interface";
 
 @Component({
-  selector: 'side-bar-cart',
-  templateUrl: './side-bar-cart.component.html',
-  styleUrls: ['./side-bar-cart.component.scss']
+  selector: "side-bar-cart",
+  templateUrl: "./side-bar-cart.component.html",
+  styleUrls: ["./side-bar-cart.component.scss"],
 })
-export class SideBarCartComponent {
-  cartItems:CartItem[] = cartItems;
-  products:Product[] = products;
-  
+export class SideBarCartComponent implements OnInit {
+  cartItems: CartItem[] = [];
+
+  products: Product[] = [];
 
   @Output() isLoad: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  
 
-  constructor() { }
+  constructor(private readonly productService: ProductsService) {
+/*     console.log("cartItem",this.cartItems);
+    console.log("cart",this.products); */
+  }
 
-  emitStateLoad(state:boolean){
+  async ngOnInit() {
+    this.setCartItems();
+  }
+
+
+  emitStateLoad(state: boolean) {
     setTimeout(() => {
       this.isLoad.emit(state); // Emite false para ocultar el spinner
     }, 2000);
+  }
+
+  getCartItems():CartWithItems {
+    const cart: CartWithItems = (JSON.parse(localStorage.getItem("cart"))).cart;
+    return cart; 
+  }
+
+  setCartItems(){
+    this.cartItems = this.getCartItems().items
+  }
+
+  async getProductById(id: number) {
+    this.productService
+      .getById(id)
+      .then((res: ProductsResponse) => {
+        if (res.error && res.error.errorType === eErrorType.None) {
+          console.error(`Error: ${res.error.errorType}, ${res.error.message}`);
+          return;
+        }
+
+        this.products.push(res.product);
+      })
+      .catch(error => {
+        console.error("Error al obtener los productos: ",error);
+      });
+  }
+
+
+  calculatePrice():number{
+    return this.getCartItems().items.reduce((total, item) => {
+      return total + (item.price * item.quantity);
+    }, 0);
   }
 }
--- sitic-commerce/src/app/modules/store/side-bar-item/side-bar-item.component.ts
+++ sitic-commerce/src/app/modules/store/side-bar-item/side-bar-item.component.ts
@@ -1,29 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CartItem } from 'src/app/shared/interfaces/carts/cart-item/cart-item.interface';
-import { Product } from 'src/app/shared/interfaces/products/product.interface';
-import {products} from './data-dummie'
+import { Component, Input, OnInit } from "@angular/core";
+import { CartItem } from "src/app/shared/interfaces/carts/cart-item/cart-item.interface";
+import { Product } from "src/app/shared/interfaces/products/product.interface";
+import { ProductsService } from "src/app/shared/services/products.service";
+import { eErrorType } from "src/app/shared/interfaces/comun/enums.interface";
+import { ProductsResponse } from "src/app/shared/interfaces/products/products-response.interface";
 
 @Component({
-  selector: 'side-bar-item',
-  templateUrl: './side-bar-item.component.html',
-  styleUrls: ['./side-bar-item.component.scss']
+  selector: "side-bar-item",
+  templateUrl: "./side-bar-item.component.html",
+  styleUrls: ["./side-bar-item.component.scss"],
 })
 export class SideBarItemComponent implements OnInit {
+  /*  @Input() product:Product; */
+  @Input() cartItem: CartItem;
+  product: Product;
+  error: boolean = false;
 
-  products:Product[] = products;
-  @Input() cartItem:CartItem;
-  product:Product;
-  error:boolean = false;
-
-  constructor() {
-  }
+  constructor(private readonly productService: ProductsService) {}
 
   ngOnInit(): void {
+    console.log("side bar item: ", this.product);
+    console.log("side bar item: ", this.cartItem);
     this.getProduct(this.cartItem.product_Id);
   }
 
-  getProduct(itemId:number){
-    this.product= this.products.find(product=>product.id=itemId);
-  }
+  async getProduct(itemId: number) {
+    try {
+      const product:ProductsResponse = await this.productService.getById(itemId);
+      if (product.error && product.error.errorType === eErrorType.None) {
+        console.error(`Error: ${product.error.errorType}, ${product.error.message}`);
+        return;
+      }
 
+      this.product = product.product;
+      console.log(product.product);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
